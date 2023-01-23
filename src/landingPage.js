1fea7c7dd4f928de0283da74c7bb33f01e119a31
BenZ9413 <benedikt.sell@yahoo.de>
2023-01-23 06:31:40 +0100

    added header, main and footer and button for new tasks

--- src/landingPage.js
+++ src/landingPage.js
@@ -1,5 +1,36 @@
+const body = document.querySelector("body");
+
 const loadLandingPage = function () {
-  console.log("Loading landing page...");
+  createHeader();
+  createMain();
+  createFooter();
+};
+
+const createHeader = function () {
+  const header = document.createElement("header");
+  const pageTitle = document.createElement("div");
+  pageTitle.setAttribute("id", "pageTitle");
+  pageTitle.textContent = "Masterlist";
+  header.appendChild(pageTitle);
+  body.appendChild(header);
+};
+
+const createMain = function () {
+  const main = document.createElement("main");
+  const btnNewTask = document.createElement("button");
+  btnNewTask.setAttribute("id", "btnNewTask");
+  btnNewTask.textContent = "+";
+  main.appendChild(btnNewTask);
+  body.appendChild(main);
+};
+
+const createFooter = function () {
+  const footer = document.createElement("footer");
+  const copyright = document.createElement("div");
+  copyright.setAttribute("id", "copyright");
+  copyright.textContent = "created by BenZ9413";
+  footer.appendChild(copyright);
+  body.appendChild(footer);
 };
 
 export default loadLandingPage;
