434f06d5e9f545eea61d3c83d722655d26f38e42
BenZ9413 <benedikt.sell@yahoo.de>
2023-01-24 05:27:29 +0100

    added click event to newTask and created popUp Module

--- src/btnEventHandler.js
+++ src/btnEventHandler.js
@@ -1,5 +1,12 @@
+import showTaskPopUp from "./popUp";
+
 const addButtonFunctionalityToLandingPage = function () {
-  alert("TaskPopUp");
+  btnNewTaskClick();
+};
+
+const btnNewTaskClick = function () {
+  const btnNewTask = document.querySelector("#btnNewTask");
+  btnNewTask.addEventListener("click", showTaskPopUp);
 };
 
 export default addButtonFunctionalityToLandingPage;
---
+++ src/popUp.js
@@ -0,0 +1,13 @@
+const createTaskPopUp = function () {
+  alert("Create");
+};
+
+const showTaskPopUp = function () {
+  alert("Task PopUp");
+};
+
+const hideTaskPopUp = function () {
+  alert("Hide");
+};
+
+export default showTaskPopUp;
