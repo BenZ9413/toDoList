2b9487880d1202230c28a3ea4736589d2799457d
BenZ9413 <benedikt.sell@yahoo.de>
2023-01-28 17:24:18 +0100

    extract formValues and create new project and new task if project doesnt exist yet

--- src/toDoListManager.js
+++ src/toDoListManager.js
@@ -1,23 +1,49 @@
 import masterToDoList from "./toDoListObject";
 
+const formValues = {};
+
 const saveOrUpdateTask = (e) => {
-  alert("Saving or updating...");
-  console.log(e.target.form);
+  extractFormValues(e);
+  saveProjectAndTaskIfProjectIsNew();
+  // addProjectIfNew(form.elements["project"].value);
+  // console.log(form.elements["formTask"].value);
+  // alert("Saving or updating...");
+  // console.log(e.target.form);
+  // console.log(masterToDoList);
 };
 
-const addProject = (projectName) => {
-  const projectExists = checkIfProjectAlreadyExists(projectName);
-  if (!projectExists) masterToDoList.listOfProjects.projectName = projectName;
+const extractFormValues = (e) => {
+  const form = e.target.form;
+  const listOfInputs = form.querySelectorAll("input");
+  for (let i = 0; i < listOfInputs.length; i++) {
+    formValues[`${listOfInputs[i].name}`] = listOfInputs[i].value;
+  }
 };
 
-const checkIfProjectAlreadyExists = (projectName) => {
+const saveProjectAndTaskIfProjectIsNew = () => {
+  if (projectExists()) return;
+  addProject();
+  addTaskToProject();
+  console.log(masterToDoList);
+};
+
+const projectExists = () => {
   for (const projects in masterToDoList.listOfProjects) {
-    if (masterToDoList.listOfProjects[projects] == projectName) return true;
+    if (masterToDoList.listOfProjects[projects] == formValues["project"])
+      return true;
   }
   return false;
 };
 
-const addTaskToProject = () => {};
+const addProject = () => {
+  masterToDoList.listOfProjects[`${formValues["project"]}`] = {};
+};
+
+const addTaskToProject = () => {
+  masterToDoList.listOfProjects[`${formValues["project"]}`][
+    `${formValues["task"]}`
+  ] = {};
+};
 
 const removeProject = (projectName) => {
   console.log("removeProject");
