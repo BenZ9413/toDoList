7c59c3f020afca387fbd37e69d83e8634c89252d
BenZ9413 <benedikt.sell@yahoo.de>
2023-02-02 05:42:35 +0100

    added checked property and method to toggle it

--- src/taskObject.js
+++ src/taskObject.js
@@ -3,12 +3,21 @@ class Task {
     this.priority = "";
     this.dueDate = "";
     this.description = "";
+    this.checked = false;
   }
 
   updateTaskDetails = (newPriority, newDueDate, newDescription) => {
     this.priority = newPriority;
     this.dueDate = newDueDate;
     this.description = newDescription;
+  };
+
+  toggleChecked = () => {
+    if (this.checked == false) {
+      this.checked = true;
+    } else {
+      this.checked = false;
+    }
   };
 }
 
