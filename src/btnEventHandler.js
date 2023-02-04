28f231b77e2fafea269174062aab6868b1b2ce2c
BenZ9413 <benedikt.sell@yahoo.de>
2023-02-04 10:28:28 +0100

    removed not needed variable

--- src/btnEventHandler.js
+++ src/btnEventHandler.js
@@ -73,7 +73,7 @@ const addEventListenerToAllUpdateButtons = () => {
 };
 
 const showPopUpBufferCurrentTaskAndDeleteItFromToDoListObject = (e) => {
-  const oldTaskValues = showTaskPopUpWriteWithTaskValues(e);
+  showTaskPopUpWriteWithTaskValues(e);
   removeOldEventListenerFromCancelBtnPopUp();
   addNewEventListenerToCancelBtnPopUp();
   deleteTaskFromProject(e);
